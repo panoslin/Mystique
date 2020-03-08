a237ca55cd33096b672cfdd25a1a0e480a48ecde
Panos <panoslin@foxmail.com>
2020-03-08 18:50:40 +0800

    init config

---
+++ config.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Created by panos on 2020/3/8
+# IDE: PyCharm
+
+
+resolution = {
+    '240': {'width': 428, 'height': 240},
+    '360': {'width': 640, 'height': 360},
+    'sd': {'width': 848, 'height': 480},
+    'xga': {'width': 1024, 'height': 576},
+    'hd': {'width': 1280, 'height': 720},
+    'fullhd': {'width': 1920, 'height': 1080},
+    '2k': {'width': 2560, 'height': 1440},
+    '4k': {'width': 3840, 'height': 2160},
+    '5k': {'width': 5120, 'height': 2880},
+    '8k': {'width': 7680, 'height': 4320},
+    '8k+': {'width': 428, 'height': 240}
+}
